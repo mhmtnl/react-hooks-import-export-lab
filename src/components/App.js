9bf73ec569f3a3aa7684b27988d420e5140dbc3f
mehmetunlu <munlu4037@gmail.com>
2023-06-16 01:00:39 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About"
+import user from "../data/user"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.username} city={user.city} />
+      <About image = {user.image}/>
     </div>
   );
 }
